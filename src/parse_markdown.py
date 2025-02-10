88772622a322bd29e2ed09cd2b042d8fbda31e48
ddhouse62 <house.dalton62@gmail.com>
2025-02-09 23:26:26 -0600

    Adds support for splitting text nodes by delimiter

---
+++ src/parse_markdown.py
@@ -0,0 +1,22 @@
+from sys import exception
+from htmlnode import *
+from textnode import *
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            split_nodes = node.split(delimiter)
+            if len(split_nodes) % 2 == 0:
+                raise ValueError("Invalid Markdown, formatted section not closed")
+            for i in range(len(split_nodes)):
+                if split_nodes[i] == "":
+                    continue
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(split_nodes[i], TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(split_nodes[i], TextType.text_type))
+                    
+    return new_nodes
