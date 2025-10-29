caac44bfbd06e3cc9291be24d86a22ad540fd3c2
Dalton House <house.dalton62@gmail.com>
2025-10-29 18:23:26 -0500

    Starts creation of html_nodes

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -48,3 +48,17 @@ def block_to_block_type(block):
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+# TODO - Finish Markdown to HTML Node function
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+# TODO - Finish HTML To Block Node - Takes a block (text string) and blocktype to return a html node
+def html_block_node(block, block_type):
+    match block_type:
+        case BlockType.HEADING:
+            heading = block.split(" ")[0]
+            heading_level = heading.count("#")
+                            
