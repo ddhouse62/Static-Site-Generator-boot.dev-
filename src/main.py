fa1d0897eea161466343075354a31a19bea642c1
Dalton House <house.dalton62@gmail.com>
2026-05-27 16:26:09 -0500

    Adds functionality to copy all files, directories, and nested files to destination directory

--- src/main.py
+++ src/main.py
@@ -1,54 +1,23 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from parse_markdown import *
 from text_to_textnode import text_to_textnode
 from markdown_blocks import *
+from website import copy_static
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 def main():
-    '''
-    new_node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
     
-    
-
-    node1 = HTMLNode("tag", "value", ["child1", "child2"], {"prop": "https://boot.dev", "prop2": "https://example.com"})
-    
-
-    node2 = LeafNode("a", "Value1", {"href": "https://google.com"})
-    node2.to_html()
-    
-    parent_node = ParentNode("p", [node2, LeafNode("b", "Click Here"), ParentNode("a", [LeafNode("i", "Value2")])])
-    #print(parent_node.to_html())
-
-    print(text_node_to_html_node(new_node).to_html())
-    '''
-
-    '''test_node = TextNode(
-        "![img1](link1) Middle text ![img2](link2)",
-        TextType.TEXT
-    )
-    test_split_nodes = split_nodes_image([test_node])
-    for node in test_split_nodes:
-        print(node.text)
-    '''
-    
-    sample_markdown = """# This is a heading
-
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
-
-* This is the first list item in a list block
-* This is a list item
-* This is another list item"""
-
-    md = """
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
-    """
-
-    print(markdown_to_html_node(md).to_html())
-
-
-
+    print("Copying static files to public directory...")
+    copy_static(dir_path_static, dir_path_public)
 
 
 
